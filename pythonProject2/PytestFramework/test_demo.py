7e3193a210b9dde6080520b59a7792acf4fb04ce
vijay <vijaybhaskarreddy960@gmail.com>
2023-04-03 09:50:08 +0530

    New Pytestfile

---
+++ pythonProject2/PytestFramework/test_demo.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_Amazon():
+    print("This is Amazon Method")
+
+@pytest.mark.skip
+def test_Flipkart():
+    print("This is Flipkart Method")
+
+
+def test_Snapdeal():
+    print("This is Snapdeal Method")
+
+
+def test_Myntra():
+    print("This is Myntra Method")

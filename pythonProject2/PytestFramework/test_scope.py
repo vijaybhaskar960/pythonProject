df32b79f92708bca9e3c590b6422769eb16602c0
vijay <vijaybhaskarreddy960@gmail.com>
2023-04-09 11:28:14 +0530

    Scope file

---
+++ pythonProject2/PytestFramework/test_scope.py
@@ -0,0 +1,19 @@
+import pytest
+
+@pytest.fixture(scope="module")
+def connect_db():
+    print("I need to connect employee database")
+    yield
+    print("I need to Disconnect employee database")
+
+
+def test_first(connect_db):
+    print("I am Verify the test first employee name and id")
+
+
+def test_Second(connect_db):
+    print("I am Verify the test first employee name and id")
+
+
+def test_Third(connect_db):
+    print("I am Verify the test first employee name and id")

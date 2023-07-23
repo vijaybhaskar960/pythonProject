192bde79d6e69c9608e1145f2e5509be923606ac
vijay <vijaybhaskarreddy960@gmail.com>
2023-07-23 08:56:24 +0530

    Test commit

---
+++ pythonProject2/Selenium/AdExtensions.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+# options.add_experimental_option('detach',True)
+options.add_extension('AdBloker.crx')
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+driver.get("https://www.amazon.in/")
+driver.maximize_window()
+time.sleep(10)
